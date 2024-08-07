6b9a2f6c0cae9b4cc44adec30b864fc3d74ab5d0
Jon Addams <jonaddams@gmail.com>
2024-08-07 13:49:37 -0400

    Add license key

--- src/app/viewer.jsx
+++ src/app/viewer.jsx
@@ -22,6 +22,8 @@ export default function Viewer({ document }) {
       const pspdfkitConfig = {
         container,
         baseUrl: pspdfkitBaseUrl,
+        licenseKey:
+          "sdERXsKZP49pNIWD061LXpZFWHJVkjmffvHHNDb-ruCYovkLg7irqylmzYpLB5fmPM8wVXOnQPRWG-TNUhMrNbaJ1bhYF4WeY2vv7MqmWZkf_k_taO-Vf_S4E5LVIU7b23OMZWGIO0nIiTzmtLSemMZwjnyAdDx2itDmaVzFSXQy0UHu4IfZK-xPBAIGuIkTFgsnlIOdb6Vk0hXo8kzkwilCyHrZegVgRPBsU7gVWlCRxhRUIB-TuWns8EL6jRknEmTVyXCdCNt6pEsONRbvuBMzzXzqwFY8JImuhcfrBEsiVkQToevRDtYps58bQUNVp101OEfa_hMtkgvyeWEDAFd9BWuh9Aqly0KOae3mYkRDbvdX2r4GwTWYY8FqG2oZXryGU3aJD3_KMrOooo-d0nEfP1-7BU0AJl_XY0_00vd5VadhxPSKC8lhW-ADdLNLY4DqG4g6pcMc47y_g0TbUGa_GO6po6SOcs4HgzSeQvqFPvhCTrstuTSpr4GTPDJRlIVf32M_bn0P51DixISn5jqX_D-umIAlmf7Nmo0xZNAQbnaSh57cg-Dp70PfqvFWh5nQb3o1pCK73QbGlx_2MOGDUjQg_DmCzr0iCUSJRzCiOosi6rk43oruoa_3dj2KfcEstS93S4X9I6E2cG3iGeJUj3FdAbQ4Cm7t_UwRx5xntUcn40G6luXPuz5-VDapnSOwGqYgj6F6C3BiMziFFHJzJmcz0Xd_me3C-3W3voRrS9_krs3VP_Mf4zU8tsMeGW0VTOucEIaEWq2-OR31YuzaRE_u_lo2i2xm2Pt1W9xgj3SRFBu-NQG9fXas-6iAtnJg4aoYpl5y1uBOLbTmugt-L6ruxkALnZGdz0TnlJ95Hts8mZcqB5Kp6GFtNvXOKqE6IZuIXeUN1fg3qG1Plg==",
         styleSheets: ["/pspdfkit.css"],
         ProcessorEngine: "fasterProcessing",
         // https://pspdfkit.com/api/web/PSPDFKit.html#.ProcessorEngine
